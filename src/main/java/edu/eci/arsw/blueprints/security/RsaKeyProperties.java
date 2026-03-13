4a0df292aa697ea78e9229303436d13cbece261f
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2026-03-12 19:12:47 -0500

    Sincronizando estado actual: reflejando archivos borrados y cambios locales

--- src/main/java/edu/eci/arsw/blueprints/security/RsaKeyProperties.java
+++
@@ -1,6 +0,0 @@
-package edu.eci.arsw.blueprints.security;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "blueprints.security")
-public record RsaKeyProperties(String issuer, Integer tokenTtlSeconds) {}
